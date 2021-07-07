fc1763396831403e4389a10b70114f07455e053a
Ratheshan03 <lionratheshan@gmail.com>
2021-07-07 18:33:32 +0530

    Uploading Tiktok-clone

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBXDDGAIn7WJJlR4lku5oT5XZHhWGw0gj0",
     authDomain: "tiktok-clone-ddc29.firebaseapp.com",
@@ -7,3 +9,9 @@ const firebaseConfig = {
     appId: "1:408611980039:web:22fa12db034c3c0dc53ba2",
     measurementId: "G-L0HDG2GLJQ"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
